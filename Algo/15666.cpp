4db09ee7b63deb9e80bc24887b3749caf70bd63b
jason967 <jason967@naver.com>
2020-07-28 23:10:51 +0900

    0728

---
+++ Algo/15666.cpp
@@ -0,0 +1,39 @@
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+vector<int> V;
+vector<vector<int>> Ans;
+int A[8], N, M;
+bool C[8];
+
+void go(int cnt)
+{
+	if (cnt == M)
+	{
+		Ans.push_back(V);
+		return;
+	}
+	for (int i = 0; i < N; i++)
+	{
+		if (cnt != 0 && V[cnt - 1] > A[i]) continue;
+		V.push_back(A[i]);
+		go(cnt + 1);
+		V.pop_back();
+	}
+}
+int main()
+{
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) scanf("%d", &A[i]);
+	sort(A, A + N);
+	go(0);
+	sort(Ans.begin(), Ans.end());
+	Ans.erase(unique(Ans.begin(), Ans.end()), Ans.end());
+	for (auto x : Ans)
+	{
+		for (int i = 0; i < x.size(); i++) printf("%d ", x[i]);
+		printf("\n");
+	}
+}

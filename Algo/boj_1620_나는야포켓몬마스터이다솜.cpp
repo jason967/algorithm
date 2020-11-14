1962f8ba2c37a801a996a9f1a98e85079201701e
최재웅 <jason967@naver.com>
2020-11-15 02:31:01 +0900

    1115

---
+++ Algo/boj_1620_나는야포켓몬마스터이다솜.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N, M;
+	cin >> N >> M;
+	map<string, int> m;
+	vector<string> V;
+	V.push_back("temp");
+	for (int i = 1; i <= N; i++)
+	{
+		string S;
+		cin >> S;
+		m.insert({ S,i });
+		V.push_back(S);
+	}
+	for (int i = 1; i <= M; i++)
+	{
+		string s;
+		cin >> s;
+		if (s[0] >= '0'&&s[0] <= '9')
+		{
+			cout << V[stoi(s)] << '\n';
+		}
+		else
+		{
+			cout << m[s] << "\n";
+		}
+	}
+}
